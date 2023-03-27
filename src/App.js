32b6610ebefe1e71312d7e929718ee1524e71637
Saarthak Slathia <77338782+Saarthak-Slathia@users.noreply.github.com>
2023-03-27 15:01:00 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+import ProductsPage from "./components/ProductsPage";
+import Footer from "./components/Footer";
+import {createContext, useState} from "react";
+
+export const CartContext = createContext();
 
 function App() {
+  const [cart, setCart] = useState(0);
+  const stateProvider = {cart, setCart};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <CartContext.Provider value={stateProvider} className="App">
+      <Navbar />
+      <Landing />
+      <ProductsPage />
+      <Footer />
+    </CartContext.Provider>
   );
 }
 
